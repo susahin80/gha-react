ad6c240d196fed9424e8435c173d1f2ff3ef48eb
susahin80 <susahin80@outlook.com.tr>
2023-05-01 20:52:20 +0300

    feat: semantic release step added 3

--- release.config.js
+++ release.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   branches: ["main"],
-  repositoryUrl: "https://github.com/susahin80/gha-react.git",
+  repositoryUrl: "https://github.com/susahin80/gha-react",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
     "@semantic-release/github",
   ],
 };
