794facba45af806f8008ab019703be3b512ea1cb
susahin80 <susahin80@outlook.com.tr>
2023-05-02 12:19:17 +0300

    fix: open issue 4

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const el = screen.getByText(/learn react/i);
+  const el = screen.getByText(/learn react xxx/i);
   expect(el).toBeInTheDocument();
 });
